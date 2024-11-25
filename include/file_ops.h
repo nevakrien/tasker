c0212dd4dca0d6f54046dd0e72cb3e6862df2e09
kycasp <96327019+kycasp@users.noreply.github.com>
2024-11-25 01:56:13 -0600

    Add typedef to CopyTask
    removed any struct keywords after applying typedef keyword

--- include/file_ops.h
+++ include/file_ops.h
@@ -4,13 +4,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct CopyTask {
+typedef struct
+{
     const char *in_path;
     const char *out_path;
     volatile bool *global_err;
-};
+} CopyTask;
 
-int copy_file(struct CopyTask* task);
+int copy_file(CopyTask *task);
 int parallel_copy_file(const char **in, const char **out, size_t num_files);
 
 #endif // FILE_OPS_H
