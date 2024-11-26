4ead72d96f0c78776ee35a7cf216d8b67e981df1
User <nevo.krien@gmail.com>
2024-11-27 00:00:46 +0200

    api is weird man

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 CC = cc
 
 # Compiler flags (you can adjust as needed)
-CFLAGS = -Wall -Wextra -O2 --std=gnu99 -Iinclude
+CFLAGS = -Wall -Wextra -g3 --std=gnu99 -Iinclude
 
 all: hello_world 
 
@@ -11,11 +11,12 @@ hello_world: src/hello_world.c
 	$(CC) $(CFLAGS) -o bin/hello_world src/hello_world.c
 
 
-run: src/maker.c
+run: src/maker.c clean
 	 $(CC) $(CFLAGS) src/maker.c && ./a.out && ./bin/hello_world
 
 # Clean up the build files
 clean:
-	rm bin/*
+	rm -rf bin
+	mkdir bin
 
 .PHONY: clean run
--- src/maker.c
+++ src/maker.c
@@ -2,51 +2,49 @@
 #include <stdint.h>
 
 static inline System2CommandInfo make_info(){
-	System2CommandInfo info = {0};
-	// info.RedirectInput = true;
-    // info.RedirectOutput = true;
+	System2CommandInfo info; //= {0};
+	memset(&info, 0, sizeof(System2CommandInfo));
+	info.RedirectInput = true;
+    info.RedirectOutput = true;
     return info;
 }
 
 static inline void abord_on_fail(SYSTEM2_RESULT result){
 	if(result!=SYSTEM2_RESULT_SUCCESS){
-		printf("failed to make subprocess %d\n",result);
+		printf("system2 failed %d\n",result);
 		fflush(stdout);
 		exit(1);
 	}
 }
 
-int main(int argc, char** argv)  {
+int main()  {
 	printf("started\n");
 
 	System2CommandInfo info = make_info();
 	const char* args[] = {"gcc","-g3","-std=c99","-Wall","src/hello_world.c","-obin/hello_world"}; 
 	SYSTEM2_RESULT result=System2RunSubprocess("gcc",args,sizeof(args),&info);
-	// SYSTEM2_RESULT result=System2Run("gcc -g3 -std=c99 src/hello_world.c -obin/hello_world",&info);
 	abord_on_fail(result);
-
-
-	int return_code;
-	result = System2GetCommandReturnValueSync(&info,&return_code);
-	abord_on_fail(result);
-
-	if(return_code){
-		printf("got return code %d\n",return_code);
-	}
 
 	uint32_t bytes_read = 0;
 	char buffer[BUFSIZ] = {0};
 	do {
-		SYSTEM2_RESULT result =  System2ReadFromOutput(&info,buffer,BUFSIZ,&bytes_read);
-		for (int i = 0; i < bytes_read; i++) {
+		result =  System2ReadFromOutput(&info,buffer,BUFSIZ,&bytes_read);
+		for (uint32_t i = 0; i < bytes_read; i++) {
 	        putchar(buffer[i]);
 	    }
 			
 	} while(result == SYSTEM2_RESULT_READ_NOT_FINISHED);
 
 	abord_on_fail(result);
-	printf("yay\n");
 
+	int return_code;
+	result = System2GetCommandReturnValueSync(&info,&return_code);
+	if(return_code){
+		printf("got return code %d\n",return_code);
+	}
+	abord_on_fail(result);
+
+	printf("yay\n");
 	return 0;
 	
 }
